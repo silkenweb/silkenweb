7612b4f1c630f044ac88fe75500ffcfc2d50dd72
Simon Bourne <simonbourne@gmail.com>
2021-03-31 23:10:31 +0100

    Basic state updates are working

--- crates/lib/src/lib.rs
+++ crates/lib/src/lib.rs
@@ -1,121 +1,166 @@
-use std::{
-    borrow::BorrowMut,
-    cell::{RefCell, RefMut},
-    rc::Rc,
-};
+use std::{cell::Cell, rc::Rc};
 
-pub struct Attribute {
-    name: String,
-    value: String,
+pub fn tag(name: impl ToString) -> ElementBuilder {
+    ElementBuilder::new(name)
 }
 
-// TODO: Make private
-pub struct ConcreteElement {
-    tag: String,
-    attributes: Vec<Attribute>,
-    children: Vec<Element>,
+pub struct Element {
+    dom_element: DomElement,
+    states: Vec<Box<dyn AnyState>>,
 }
 
-pub struct IsUpdated(bool);
-
-pub trait AnyState {
-    fn eval(&self) -> Element;
-
-    fn update(&mut self) -> IsUpdated;
-}
-
-pub enum Element {
-    // TODO: Should this be a DOM element?
-    Concrete(ConcreteElement),
-    NewState(Box<dyn AnyState>),
+impl Element {
+    // TODO: Don't allow
+    #[allow(dead_code)]
+    fn update(&self) {
+        for state in &self.states {
+            state.update()
+        }
+    }
 }
 
 pub struct ElementBuilder {
-    tag: String,
-    attributes: Vec<Attribute>,
-    children: Vec<Element>,
+    dom_element: DomElement,
+    states: Vec<Box<dyn AnyState>>,
 }
 
 impl ElementBuilder {
     pub fn new(tag: impl ToString) -> Self {
-        Self {
-            tag: tag.to_string(),
-            attributes: Vec::new(),
-            children: Vec::new(),
+        println!("Tag {}", tag.to_string());
+        ElementBuilder {
+            dom_element: DomElement {},
+            states: Vec::new(),
         }
     }
 
-    pub fn attribute(mut self, name: impl ToString, value: impl ToString) -> Self {
-        self.attributes.push(Attribute {
-            name: name.to_string(),
-            value: value.to_string(),
-        });
+    pub fn attribute(self, name: impl ToString, value: impl ToString) -> Self {
+        println!("Attribute {} = '{}'", name.to_string(), value.to_string());
         self
     }
 
     pub fn child(mut self, child: impl Into<Element>) -> Self {
-        self.children.push(child.into());
+        println!("Adding child");
+        self.states.extend(child.into().states);
         self
     }
+
+    pub fn build(self) -> Element {
+        Element {
+            dom_element: self.dom_element,
+            states: self.states,
+        }
+    }
 }
 
-// TODO: Name? This handles updates as well
-pub struct StateReader<T> {
-    current: T,
-    next: StateWriter<T>,
+impl From<ElementBuilder> for Element {
+    fn from(builder: ElementBuilder) -> Self {
+        builder.build()
+    }
 }
 
-struct WithState<T, F> {
-    reader: StateReader<T>,
-    f: F,
-}
-
-impl<T, F> AnyState for WithState<T, F>
+pub fn state<T, E>(init: T, generate: impl 'static + Fn(T, StateSetter<T>) -> E) -> Element
 where
-    F: Fn(&StateReader<T>, StateWriter<T>) -> Element,
+    E: Into<Element>,
+    T: 'static,
 {
-    fn eval(&self) -> Element {
-        (self.f)(&self.reader, self.reader.next.clone())
-    }
+    let setter = StateSetter::default();
+    let element = generate(init, setter.clone()).into();
+    let dom_element = element.dom_element;
 
-    fn update(&mut self) -> IsUpdated {
-        let next = self.reader.next.0.as_ref().replace(None);
-
-        IsUpdated(if let Some(new) = next {
-            self.reader.current = new;
-            true
-        } else {
-            false
-        })
+    Element {
+        dom_element: dom_element.clone(),
+        states: vec![Box::new(State {
+            _dom_element: dom_element,
+            generate: move |value, setter| generate(value, setter).into(),
+            setter,
+        })],
     }
 }
 
-pub struct StateWriter<T>(Rc<RefCell<Option<T>>>);
+struct State<T, F> {
+    _dom_element: DomElement,
+    generate: F,
+    setter: StateSetter<T>,
+}
 
-impl<T> Clone for StateWriter<T> {
+pub struct StateSetter<T>(Rc<Cell<Option<T>>>);
+
+impl<T> Clone for StateSetter<T> {
     fn clone(&self) -> Self {
         Self(self.0.clone())
     }
 }
 
-impl<T> Default for StateWriter<T> {
+impl<T> Default for StateSetter<T> {
     fn default() -> Self {
-        Self(Rc::new(RefCell::new(None)))
+        Self(Rc::new(Cell::new(None)))
     }
 }
 
-pub fn state<T: 'static>(init: T, f: impl 'static + Fn(&StateReader<T>, StateWriter<T>) -> Element) -> Element
-{
-    let writer = StateWriter::default();
-    let reader = StateReader {
-        current: init,
-        next: writer,
-    };
+impl<T> StateSetter<T> {
+    pub fn set(&self, new_value: T) {
+        self.0.replace(Some(new_value));
+    }
 
-    Element::NewState(Box::new(WithState {
-        reader,
-        f,
-    }))
+    fn take(&self) -> Option<T> {
+        self.0.replace(None)
+    }
 }
 
-pub struct Dom {}
+trait AnyState {
+    fn update(&self);
+}
+
+impl<T, F> AnyState for State<T, F>
+where
+    F: Fn(T, StateSetter<T>) -> Element,
+{
+    fn update(&self) {
+        let change = self.setter.take();
+
+        if let Some(new_value) = change {
+            (self.generate)(new_value, self.setter.clone());
+            println!("Replacing node");
+        }
+    }
+}
+
+#[derive(Clone)]
+struct DomElement;
+
+#[cfg(test)]
+mod tests {
+    use crate::{state, tag, ElementBuilder};
+
+    fn parent() -> ElementBuilder {
+        tag("tag_name").attribute("attr_name", "attr_value")
+    }
+
+    fn child(i: i32) -> ElementBuilder {
+        tag("child_tag_name").attribute("child_attr_name", format!("{}", i))
+    }
+
+    #[test]
+    fn simple() {
+        parent().child(child(0)).build();
+    }
+
+    #[test]
+    fn state_unchanged() {
+        let element = parent().child(state(0, |i, _set_i| child(i))).build();
+        println!("Updating");
+        element.update();
+    }
+
+    #[test]
+    fn state_changed() {
+        let element = parent()
+            .child(state(0, |i, set_i| {
+                set_i.set(1);
+                child(i)
+            }))
+            .build();
+        println!("Updating");
+        element.update();
+    }
+}
