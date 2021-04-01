e28b695983e06d2b3acb720224d3c14d66d463d9
Simon Bourne <simonbourne@gmail.com>
2021-04-02 00:17:53 +0100

    More complex example

--- crates/exe/src/main.rs
+++ crates/exe/src/main.rs
@@ -7,11 +7,21 @@ fn main() {
     tag("div")
         .attribute("id", "hello-world!")
         .child(state(0, |i, set_i| {
-            if i < 10 {
+            if i < 100 {
                 set_i.set(i + 1)
             };
 
-            tag("p").text(format!("Count = {}", i))
+            // TODO: Make text a free function as well.
+            let mut counters = tag("p").text(format!("Count = {}", i));
+
+            for j in 0..i {
+                counters = counters.child(state(j, |j, set_j| {
+                    set_j.set(j + 1);
+                    tag("p").text(format!("Count = {}", j))
+                }))
+            }
+
+            counters
         }))
         .build()
         .append_to_body();
