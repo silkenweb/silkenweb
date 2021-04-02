d50119a17edf936db440039e7a0c2f22b09c4634
Simon Bourne <simonbourne@gmail.com>
2021-04-02 21:46:40 +0100

    More complex counting example

--- crates/exe/src/main.rs
+++ crates/exe/src/main.rs
@@ -1,4 +1,4 @@
-use lib::{ElementBuilder, state, tag, StateSetter};
+use lib::{state, tag, ElementBuilder, StateSetter};
 
 fn counter(i: u64, set_i: StateSetter<u64>) -> ElementBuilder {
     let inc_i = set_i.clone();
@@ -22,5 +22,14 @@ fn main() {
     console_error_panic_hook::set_once();
     web_log::println!("Running");
 
-    state(0, |i, set_i| counter(i, set_i)).append_to_body();
+    state(0, |i, set_i| {
+        let mut c = counter(i, set_i);
+
+        for _j in 0..i {
+            c = c.child(state(0, counter));
+        }
+
+        c
+    })
+    .append_to_body();
 }
