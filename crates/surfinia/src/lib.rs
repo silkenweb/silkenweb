04cfa7ad7c96cb277d49d823116990cf7728fa36
Simon Bourne <simonbourne@gmail.com>
2021-04-03 22:23:53 +0100

    Cargo fmt

--- crates/surfinia/src/lib.rs
+++ crates/surfinia/src/lib.rs
@@ -1,2 +1,2 @@
-pub use surfinia_core::{append_to_body, tag, Builder, Element, ElementBuilder, State, Setter};
+pub use surfinia_core::{append_to_body, tag, Builder, Element, ElementBuilder, Setter, State};
 pub use surfinia_html::{button, div, Button, ButtonBuilder, Div, DivBuilder};
