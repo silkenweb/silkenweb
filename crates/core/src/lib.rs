eb5fb4b286a5fbcedebbf57c8a7112b4c9e6e47c
Simon Bourne <simonbourne@gmail.com>
2021-04-11 20:50:27 +0100

    Implement Ref and RefMut proxy methods

--- crates/core/src/lib.rs
+++ crates/core/src/lib.rs
@@ -1,7 +1,11 @@
-#![allow(clippy::missing_panics_doc, clippy::must_use_candidate)]
+#![allow(
+    clippy::missing_panics_doc,
+    clippy::missing_errors_doc,
+    clippy::must_use_candidate
+)]
 use std::{
     any::{Any, TypeId},
-    cell::{Cell, Ref, RefCell, RefMut},
+    cell::{BorrowError, BorrowMutError, Cell, Ref, RefCell, RefMut},
     collections::HashMap,
     hash::Hash,
     marker::PhantomData,
@@ -302,7 +306,37 @@ impl<T> Reference<T> {
         RefMut::map(self.0.borrow_mut(), |x| &mut x.value)
     }
 
-    // TODO: Other method proxies from RefCell
+    pub fn replace(&self, new: T) -> T {
+        mem::replace(&mut self.0.borrow_mut().value, new)
+    }
+
+    pub fn replace_with<F>(&self, f: F) -> T
+    where
+        F: FnOnce(&mut T) -> T,
+    {
+        let old = &mut self.0.borrow_mut().value;
+        let new = f(old);
+        mem::replace(old, new)
+    }
+
+    pub fn take(&self) -> T
+    where
+        T: Default,
+    {
+        mem::take(&mut self.0.borrow_mut().value)
+    }
+
+    pub fn try_borrow(&self) -> Result<Ref<'_, T>, BorrowError> {
+        self.0
+            .try_borrow()
+            .map(|borrowed| Ref::map(borrowed, |x| &x.value))
+    }
+
+    pub fn try_borrow_mut(&self) -> Result<RefMut<'_, T>, BorrowMutError> {
+        self.0
+            .try_borrow_mut()
+            .map(|borrowed| RefMut::map(borrowed, |x| &mut x.value))
+    }
 }
 
 impl<T: 'static> Scopeable for Reference<T> {
