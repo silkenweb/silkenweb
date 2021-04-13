71d4f2f72da4894809d0192b9d9c4cd9c1a63920
Simon Bourne <simonbourne@gmail.com>
2021-04-13 15:37:34 +0100

    Put count text between buttons

--- crates/examples/incremental-counters/src/main.rs
+++ crates/examples/incremental-counters/src/main.rs
@@ -1,16 +1,19 @@
 use std::iter;
 
-use surfinia::{button, div, mount, use_list_state, use_state, DivBuilder, ElementBuilder};
+use surfinia::{button, div, mount, use_list_state, use_state, Div, ElementBuilder};
 
-fn counter() -> DivBuilder {
+fn counter() -> Div {
     let (count, set_count) = use_state(0);
-    let inc = set_count.clone();
-    let dec = set_count;
 
-    div()
-        .child(button().on_click(move || inc.map(|i| i + 1)).text("+"))
-        .child(button().on_click(move || dec.map(|i| i - 1)).text("-"))
-        .child(count.with(|i| div().text(format!("Count = {}", i))))
+    count.with(move |i| {
+        let inc = set_count.clone();
+        let dec = set_count.clone();
+
+        div()
+            .child(button().on_click(move || dec.map(|i| i - 1)).text("-"))
+            .text(format!("{}", i))
+            .child(button().on_click(move || inc.map(|i| i + 1)).text("+"))
+    })
 }
 
 fn main() {
@@ -22,8 +25,8 @@ fn main() {
     mount(
         "app",
         div()
-            .child(button().on_click(move || push_elem.push(())).text("+"))
             .child(button().on_click(move || pop_elem.pop()).text("-"))
+            .child(button().on_click(move || push_elem.push(())).text("+"))
             .child(list.with(move |()| counter())),
     );
 }
