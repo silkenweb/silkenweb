115a9763cc587e7f6eb6ce5febab3059a117d101
Simon Bourne <simonbourne@gmail.com>
2021-04-14 17:07:07 +0100

    Barebones for todomvc app

---
+++ crates/examples/todomvc/src/main.rs
@@ -0,0 +1,17 @@
+use surfinia::{button, div, mount, use_state};
+
+fn main() {
+    console_error_panic_hook::set_once();
+
+    let (count, set_count) = use_state(0);
+    let inc = set_count.clone();
+    let dec = set_count;
+
+    mount(
+        "app",
+        div()
+            .child(button().on_click(move || inc.map(|i| i + 1)).text("+"))
+            .child(button().on_click(move || dec.map(|i| i - 1)).text("-"))
+            .child(count.with(|i| div().text(format!("Count = {}", i)))),
+    );
+}
