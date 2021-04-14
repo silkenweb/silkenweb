9cedd4a34589c4ebfeca6b12e1c73f131c9ff98a
Simon Bourne <simonbourne@gmail.com>
2021-04-14 12:17:43 +0100

    Export Get/Set ListState

--- crates/surfinia/src/lib.rs
+++ crates/surfinia/src/lib.rs
@@ -1,10 +1,9 @@
 pub use surfinia_core::{
     hooks::{
-        // list_state::{use_list_state, GetListState, SetListState},
+        list_state::{use_list_state, GetListState, SetListState},
         memo::Memo,
         reference::Reference,
         state::{use_state, GetState, SetState},
-        list_state::use_list_state
     },
     mount,
     tag,
