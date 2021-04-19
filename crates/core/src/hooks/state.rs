0ad1efa0dce76ab1ab87cfb68cccb82733371972
Simon Bourne <simonbourne@gmail.com>
2021-04-19 23:14:39 +0100

    Remove dependent callbacks when child dropped

--- crates/core/src/hooks/state.rs
+++ crates/core/src/hooks/state.rs
@@ -1,6 +1,8 @@
 use std::{
     cell::{Ref, RefCell},
-    rc::Rc,
+    collections::HashSet,
+    hash::Hash,
+    rc::{self, Rc},
 };
 
 type SharedState<T> = Rc<RefCell<State<T>>>;
@@ -47,14 +49,21 @@ impl<T: 'static> ReadSignal<T> {
     {
         let value = Signal::new(generate(&self.current()));
 
-        // TODO: Handle removing the new value from dependents.
-        self.0.borrow_mut().dependents.push(Rc::new({
+        self.add_dependent(DependentCallback::new({
             let set_value = value.write();
 
             move |new_value| set_value.set(generate(new_value))
         }));
 
         value.read()
+    }
+
+    fn add_dependent(&self, dependent_callback: DependentCallback<T>) {
+        let mut state = self.0.borrow_mut();
+        state
+            .parents
+            .push(Box::new(Parent::new(&dependent_callback, &self)));
+        state.dependents.insert(dependent_callback);
     }
 }
 
@@ -74,24 +83,24 @@ where
     type Target = U;
 
     fn map(&self, generate: Generate) -> ReadSignal<Self::Target> {
-        let v0 = self.0.clone();
-        let v1 = self.1.clone();
-        let value = Signal::new(generate(&v0.current(), &v1.current()));
+        let x0 = &self.0;
+        let x1 = &self.1;
+        let value = Signal::new(generate(&x0.current(), &x1.current()));
         let generate0 = Rc::new(generate);
         let generate1 = generate0.clone();
 
-        // TODO: Handle removing the new value from dependents.
-        v0.0.borrow_mut().dependents.push(Rc::new({
+        x0.add_dependent(DependentCallback::new({
             let set_value = value.write();
-            let v1 = v1.clone();
+            let x1 = x1.clone();
 
-            move |new_value| set_value.set(generate0(new_value, &v1.current()))
+            move |new_value| set_value.set(generate0(new_value, &x1.current()))
         }));
 
-        v1.0.borrow_mut().dependents.push(Rc::new({
+        x1.add_dependent(DependentCallback::new({
             let set_value = value.write();
+            let x0 = x0.clone();
 
-            move |new_value| set_value.set(generate1(&v0.current(), new_value))
+            move |new_value| set_value.set(generate1(&x0.current(), new_value))
         }));
 
         value.read()
@@ -124,20 +133,78 @@ impl<T: 'static> WriteSignal<T> {
 
 struct State<T> {
     current: T,
-    dependents: Vec<Rc<dyn Fn(&T)>>,
+    parents: Vec<Box<dyn AnyParent>>,
+    dependents: HashSet<DependentCallback<T>>,
 }
 
 impl<T: 'static> State<T> {
     fn new(init: T) -> Self {
         Self {
             current: init,
-            dependents: Vec::new(),
+            parents: Vec::new(),
+            dependents: HashSet::new(),
         }
     }
 
     fn update_dependents(&self) {
         for dep in &self.dependents {
-            dep(&self.current);
+            (dep.0)(&self.current);
         }
+    }
+}
+
+trait AnyParent {}
+
+struct Parent<T> {
+    dependent_callback: rc::Weak<dyn Fn(&T)>,
+    parent: rc::Weak<RefCell<State<T>>>,
+}
+
+impl<T> Parent<T> {
+    fn new(dependent_callback: &DependentCallback<T>, parent: &ReadSignal<T>) -> Self {
+        Self {
+            dependent_callback: Rc::downgrade(&dependent_callback.0),
+            parent: Rc::downgrade(&parent.0),
+        }
+    }
+}
+
+impl<T> AnyParent for Parent<T> {}
+
+impl<T> Drop for Parent<T> {
+    fn drop(&mut self) {
+        if let (Some(dependent_callback), Some(parent)) =
+            (self.dependent_callback.upgrade(), self.parent.upgrade())
+        {
+            let removed = parent
+                .borrow_mut()
+                .dependents
+                .remove(&DependentCallback(dependent_callback));
+            assert!(removed);
+        }
+    }
+}
+
+struct DependentCallback<T>(Rc<dyn Fn(&T)>);
+
+impl<T> DependentCallback<T> {
+    fn new(f: impl 'static + Fn(&T)) -> Self {
+        Self(Rc::new(f))
+    }
+}
+
+impl<T> PartialEq for DependentCallback<T> {
+    fn eq(&self, other: &Self) -> bool {
+        // TODO: Investigate this further
+        #[allow(clippy::vtable_address_comparisons)]
+        Rc::ptr_eq(&self.0, &other.0)
+    }
+}
+
+impl<T> Eq for DependentCallback<T> {}
+
+impl<T> Hash for DependentCallback<T> {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        Rc::as_ptr(&self.0).hash(state);
     }
 }
