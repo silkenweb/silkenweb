2afb41ba33450579a2a69dcd25a3a3ab9ce11d85
Simon Bourne <simonbourne@gmail.com>
2021-04-21 12:44:26 +0100

    Start implementing filters

--- crates/core/src/hooks/list_state.rs
+++ crates/core/src/hooks/list_state.rs
@@ -1,19 +1,65 @@
-use indexmap::IndexMap;
+use std::{cell::RefCell, collections::BTreeMap, rc::Rc};
+
 use web_sys as dom;
 
+use super::state::{ReadSignal, Signal};
 use crate::{DomElement, Element, ElementBuilder};
 
-use super::state::ReadSignal;
+type SharedItem<T> = Rc<T>;
 
-struct Item<T> {
-    item: T,
+struct StoredItem<T> {
+    item: SharedItem<T>,
+    updater: ReadSignal<()>,
+}
+
+struct VisibleItem<T> {
+    item: SharedItem<T>,
     element: Element,
 }
 
-pub struct ElementList<T> {
-    root: ElementBuilder,
-    items: IndexMap<usize, Item<T>>,
+struct Storage<T> {
     generate_child: Box<dyn Fn(&T) -> Element>,
+    root: RefCell<ElementBuilder>,
+    items: RefCell<BTreeMap<usize, StoredItem<T>>>,
+    visible_items: RefCell<BTreeMap<usize, VisibleItem<T>>>,
+}
+
+impl<T> Storage<T> {
+    pub fn is_empty(&self) -> bool {
+        self.items.borrow().is_empty()
+    }
+
+    pub fn len(&self) -> usize {
+        self.items.borrow().len()
+    }
+
+    pub fn pop(&self) {
+        let mut items = self.items.borrow_mut();
+
+        if let Some((&key, _)) = items.iter().next_back() {
+            items.remove(&key);
+
+            if self.visible_items.borrow_mut().remove(&key).is_some() {
+                self.root.borrow_mut().remove_last();
+            }
+        }
+    }
+
+    pub fn remove(&self, key: usize) {
+        if self.items.borrow_mut().remove(&key).is_some() {
+            if let Some(VisibleItem { element, .. }) = self.visible_items.borrow_mut().remove(&key)
+            {
+                self.root.borrow_mut().remove_child(&element.dom_element());
+            }
+        }
+    }
+}
+
+// TODO: Parameterize on key type
+// TODO: Parameterize on storage type
+pub struct ElementList<T> {
+    storage: Rc<Storage<T>>,
+    filter: Box<dyn Fn(&T) -> ReadSignal<bool>>,
 }
 
 impl<T: 'static> ElementList<T> {
@@ -28,9 +74,13 @@ impl<T: 'static> ElementList<T> {
         GenerateChild: 'static + Fn(&T) -> Element,
     {
         let mut new = Self {
-            root,
-            items: IndexMap::new(),
-            generate_child: Box::new(generate_child),
+            storage: Rc::new(Storage {
+                generate_child: Box::new(generate_child),
+                root: RefCell::new(root),
+                items: RefCell::new(BTreeMap::new()),
+                visible_items: RefCell::new(BTreeMap::new()),
+            }),
+            filter: Box::new(|_| Signal::new(true).read()),
         };
 
         for (key, elem) in initial {
@@ -41,35 +91,69 @@ impl<T: 'static> ElementList<T> {
     }
 
     pub fn is_empty(&self) -> bool {
-        self.items.is_empty()
+        self.storage.is_empty()
     }
 
     pub fn len(&self) -> usize {
-        self.items.len()
+        self.storage.len()
     }
 
-    pub fn filter(&mut self, f: impl Fn(&T) -> ReadSignal<bool>) {}
+    pub fn insert(&mut self, key: usize, item: T) {
+        let item = Rc::new(item);
+        let updater = self.updater(key, &item);
 
-    pub fn insert(&mut self, key: usize, new_elem: T) {
-        let child = (self.generate_child)(&new_elem);
-        self.items.insert(
-            key,
-            Item {
-                item: new_elem,
-                element: child.clone(),
-            },
-        );
-        self.root.append_child(&child.dom_element());
-    }
-
-    pub fn remove(&mut self, key: usize) {
-        if let Some(item) = self.items.shift_remove(&key) {
-            self.root.remove_child(&item.element.dom_element());
-        }
+        self.storage
+            .items
+            .borrow_mut()
+            .insert(key, StoredItem { item, updater });
     }
 
     pub fn pop(&mut self) {
-        self.root.remove_last();
+        self.storage.pop()
+    }
+
+    pub fn remove(&mut self, key: usize) {
+        self.storage.remove(key)
+    }
+
+    pub fn filter(&mut self, f: impl Fn(&T) -> ReadSignal<bool>) {
+        // TODO
+    }
+
+    fn updater(&mut self, key: usize, item: &Rc<T>) -> ReadSignal<()> {
+        (self.filter)(&item).map({
+            let storage = self.storage.clone();
+            let item = item.clone();
+
+            move |&visible| {
+                if visible {
+                    let item = item.clone();
+
+                    storage
+                        .visible_items
+                        .borrow_mut()
+                        .entry(key)
+                        .or_insert_with(|| {
+                            let element = (storage.generate_child)(&item);
+
+                            // TODO: Insert child in correct place.
+                            storage
+                                .root
+                                .borrow_mut()
+                                .append_child(&element.dom_element());
+
+                            VisibleItem { item, element }
+                        });
+                } else if let Some(VisibleItem { element, .. }) =
+                    storage.visible_items.borrow_mut().remove(&key)
+                {
+                    storage
+                        .root
+                        .borrow_mut()
+                        .remove_child(&element.dom_element())
+                }
+            }
+        })
     }
 }
 
@@ -77,6 +161,6 @@ impl<T> DomElement for ElementList<T> {
     type Target = dom::Element;
 
     fn dom_element(&self) -> Self::Target {
-        self.root.dom_element()
+        self.storage.root.borrow().dom_element()
     }
 }
