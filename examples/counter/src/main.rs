86c878a49774e985d7bc6047531c35ecf7714bce
Simon Bourne <simonbourne@gmail.com>
2021-04-28 19:49:24 +0100

    Simplest example counter

---
+++ examples/counter/src/main.rs
@@ -0,0 +1,19 @@
+//! A really simple example counter for the README
+use silkenweb::{
+    elements::{button, div, p},
+    mount,
+    signal::Signal,
+};
+
+fn main() {
+    let count = Signal::new(0);
+    let set_count = count.write();
+    let inc = move |_, _| set_count.replace(|&i| i + 1);
+    let count_text = count.read().map(|i| format!("{}", i));
+
+    let app = div()
+        .child(button().on_click(inc).text("+"))
+        .child(p().text(count_text));
+
+    mount("app", app);
+}
