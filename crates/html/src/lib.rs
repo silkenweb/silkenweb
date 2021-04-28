3fd5a48138612a4a18c98d82f04e4d25243cb5d3
Simon Bourne <simonbourne@gmail.com>
2021-04-28 23:06:22 +0100

    Doc for html crate

--- crates/html/src/lib.rs
+++ crates/html/src/lib.rs
@@ -1,4 +1,17 @@
 #![allow(clippy::must_use_candidate)]
+//! Builders for HTML elements.
+//!
+//! Each HTML element has a function, a struct and a builder struct. The
+//! function is a constructor for the builder. The builder has methods for each
+//! attribute for that element, as well as methods for each event. For example:
+//!
+//! ```no_run
+//! # use silkenweb_html::elements::{a, A, ABuilder};
+//! use web_sys as dom;
+//! let link: ABuilder = a()
+//!     .href("https://example.com/")
+//!     .on_click(|event: dom::MouseEvent, link: dom::HtmlAnchorElement| {});
+//!```
 
 #[macro_use]
 pub mod macros;
