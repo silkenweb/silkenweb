2fbdab66fc03f572d036faaaf3eb5f0f3ebcdcb9
Simon Bourne <simonbourne@gmail.com>
2021-04-28 02:14:20 +0100

    Inline var

--- tutorial/nested-counters/src/main.rs
+++ tutorial/nested-counters/src/main.rs
@@ -19,9 +19,7 @@ fn define_counter_list(count: i64) -> Div {
     let mut counters = div();
 
     for _i in 0..count {
-        let child = define_counter(&Signal::new(0)).build();
-
-        counters = counters.child(child);
+        counters = counters.child(define_counter(&Signal::new(0)));
     }
 
     counters.build()
