83f4e5e713172d8ea107d1f380c04c6e8b80357c
Simon Bourne <simonbourne@gmail.com>
2021-04-28 11:05:29 +0100

    Signal example

---
+++ tutorial/counter-signal/src/main.rs
@@ -0,0 +1,19 @@
+use std::mem;
+
+use silkenweb::signal::{ReadSignal, Signal};
+
+// ANCHOR: body
+fn main() {
+    let count = Signal::new(0);
+    let print_count: ReadSignal<()> = count
+        .read()
+        .map(|&count| println!("The count is {}", count));
+
+    println!("Setting `count` to 1");
+    count.write().set(1);
+    println!("Dropping `print_count`");
+    mem::drop(print_count);
+    println!("Setting `count` to 2");
+    count.write().set(2);
+}
+// ANCHOR_END: body
