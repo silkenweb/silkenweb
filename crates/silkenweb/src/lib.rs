da77a65d55118daab247dc6d8a870f057459a286
Simon Bourne <simonbourne@gmail.com>
2021-04-28 20:28:28 +0100

    Add an intro in the main crate

--- crates/silkenweb/src/lib.rs
+++ crates/silkenweb/src/lib.rs
@@ -1,3 +1,35 @@
+//! Silkenweb is a reactive web library
+//!
+//! # Features
+//!
+//! - Fine grained reactivity using signals
+//! - No VDOM
+//! - Uses plain Rust syntax rather than a macro DSL
+//!
+//! # Example: A Simple Counter
+//!
+//! ```
+//! use silkenweb::{
+//!     elements::{button, div, p},
+//!     mount,
+//!     signal::Signal,
+//! };
+//!
+//! # fn no_exec() {
+//! fn main() {
+//!     let count = Signal::new(0);
+//!     let set_count = count.write();
+//!     let inc = move |_, _| set_count.replace(|&i| i + 1);
+//!     let count_text = count.read().map(|i| format!("{}", i));
+//!
+//!     let app = div()
+//!         .child(button().on_click(inc).text("+"))
+//!         .child(p().text(count_text));
+//!
+//!     mount("app", app);
+//! }
+//! # }
+//! ```
 pub use silkenweb_dom::{
     after_render, element_list, mount, render_updates, tag, unmount, Builder, DomElement, Element,
     ElementBuilder,
