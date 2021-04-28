48613b99886c6315bf494ebfd980273f0b7724af
Simon Bourne <simonbourne@gmail.com>
2021-04-28 02:12:14 +0100

    Remove completed TODO

--- tutorial/counter/src/main.rs
+++ tutorial/counter/src/main.rs
@@ -3,7 +3,6 @@ use silkenweb_tutorial_common::define_counter;
 
 fn main() {
     let count = Signal::new(0);
-    // TODO: Change render_ to define_
     let app = define_counter(&count);
 
     mount("app", app);
