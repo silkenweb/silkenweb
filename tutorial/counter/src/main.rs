821b8bef34e35e829a389e0e6a034e88732ede2d
Simon Bourne <simonbourne@gmail.com>
2021-04-28 01:30:48 +0100

    Tidy up basic counter

--- tutorial/counter/src/main.rs
+++ tutorial/counter/src/main.rs
@@ -6,20 +6,17 @@ use silkenweb::{
 };
 
 fn main() {
-    console_error_panic_hook::set_once();
-
     let count = Signal::new(0);
+    let count_text = count.read().map(|i| format!("{}", i));
+    let app = div()
+        .child(render_button("-", -1, count.write()))
+        .text(count_text)
+        .child(render_button("+", 1, count.write()));
 
-    mount(
-        "app",
-        div()
-            .child(update_count("-", -1, count.write()))
-            .text(count.read().map(|i| format!("{}", i)))
-            .child(update_count("+", 1, count.write())),
-    );
+    mount("app", app);
 }
 
-fn update_count(label: &str, delta: i64, set_count: WriteSignal<i64>) -> Button {
+fn render_button(label: &str, delta: i64, set_count: WriteSignal<i64>) -> Button {
     button()
         .on_click(move |_, _| set_count.replace(move |&i| i + delta))
         .text(label)
