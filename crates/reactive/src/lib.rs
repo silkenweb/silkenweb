9b6b5b32414fcd15b10d9d4897d3f9744d1676c4
Simon Bourne <simonbourne@gmail.com>
2021-04-28 18:34:44 +0100

    Document `clone!`

--- crates/reactive/src/lib.rs
+++ crates/reactive/src/lib.rs
@@ -8,6 +8,27 @@ pub mod accumulators;
 pub mod memo;
 pub mod signal;
 
+/// Clone all the identifiers supplied as arguments
+///
+/// `clone!(x, y, z);` will generate:
+/// ```
+/// # #[macro_use] extern crate silkenweb_reactive;
+/// # let (x, y, z) = (0, 0, 0);
+/// let x = x.clone();
+/// let y = y.clone();
+/// let z = z.clone();
+/// ```
+/// This is useful for capturing variables by copy in closures. For example:
+/// ```
+/// # #[macro_use] extern crate silkenweb_reactive;
+/// # let (x, y, z) = (0, 0, 0);
+/// # let signal = vec![0].into_iter();
+/// # fn do_something(x: u32, y: u32, z: u32) {}
+/// signal.map({
+///     clone!(x, y, z);
+///     move |_| do_something(x, y, z)
+/// });
+/// ```
 #[macro_export]
 macro_rules! clone{
     ($($name:ident),* $(,)?) => {
