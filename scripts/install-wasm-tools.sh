9ccc88e6840f21df837b58b006c2858ea8923bdd
Simon Bourne <simonbourne@gmail.com>
2021-04-30 10:24:10 +0100

    Cache browser test cargo installs

---
+++ scripts/install-wasm-tools.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+cargo-install() {
+    if ! command -v "$1" &> /dev/null; then
+        cargo install "$2" --version "$3"
+    fi
+}
+
+cargo-install wasm-bindgen wasm-bindgen-cli 0.2.73
+cargo-install wasm-pack wasm-pack 0.9.1
+cargo-install trunk trunk 0.10.0
