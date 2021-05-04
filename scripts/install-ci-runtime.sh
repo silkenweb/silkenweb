be732f3f665dc9c54d99b06f35b6bcadd4e99f36
Simon Bourne <simonbourne@gmail.com>
2021-05-04 12:59:41 +0100

    Attempt to speed up github actions

--- scripts/install-ci-runtime.sh
+++ scripts/install-ci-runtime.sh
@@ -11,3 +11,5 @@ cargo-install() {
 cargo-install wasm-bindgen wasm-bindgen-cli 0.2.73
 cargo-install wasm-pack wasm-pack 0.9.1
 cargo-install trunk trunk 0.10.0
+cargo-install cargo-udeps cargo-udeps 0.1.20
+cargo-install mdbook mdbook 0.4.7
