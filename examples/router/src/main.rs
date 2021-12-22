ebce9373200d54d842c954fa97a9ef2f4664b460
Simon Bourne <simonbourne@gmail.com>
2021-12-22 17:55:15 +0000

    Migrate router example to futures-signals

--- examples/router/src/main.rs
+++ examples/router/src/main.rs
@@ -1,7 +1,8 @@
 //! A minimal routing example
+use futures_signals::signal::SignalExt;
 use silkenweb::{
     elements::{button, div, p},
-    mount, router,
+    mount, router, signal,
 };
 
 fn main() {
@@ -17,6 +18,8 @@ fn main() {
                     .on_click(|_, _| router::set_url_path("/route_2"))
                     .text("Go to route 2"),
             )
-            .child(p().text(router::url().map(|url| format!("URL Path is: {}", url.pathname()))))
+            .child(p().text(signal(
+                router::url().map(|url| format!("URL Path is: {}", url.pathname())),
+            )))
     });
 }
