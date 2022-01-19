548a5d485084c2ce18efc51cda6d579c301d5cd3
Simon Bourne <simonbourne@gmail.com>
2022-01-19 16:33:04 +0000

    Use cargo xtask

--- README.tmpl.md
+++ README.tmpl.md
@@ -19,24 +19,7 @@ A library for building reactive single page web apps.
 ## Example: A Simple Counter
 
 ```rust
-//! A minimal interactive example
-use futures_signals::signal::Mutable;
-use silkenweb::{elements::html::*, prelude::*};
-
-fn main() {
-    let count = Mutable::new(0);
-    let count_text = count.signal_ref(|i| format!("{}", i));
-    let inc = move |_, _| {
-        count.replace_with(|i| *i + 1);
-    };
-
-    let app = div()
-        .child(button().on_click(inc).text("+"))
-        .child(p().text_signal(count_text));
-
-    mount("app", app);
-}
-
+{{{ include "examples/counter/src/main.rs" }}}
 ```
 
 ## Quick Start
---
+++ crates/xtask/src/main.rs
@@ -0,0 +1,69 @@
+use clap::Parser;
+use xshell::{cmd, pushd};
+use xtask_base::{
+    build_readme, ci, generate_open_source_files, run, CommonCmds, Toolchain, WorkflowResult,
+};
+
+#[derive(Parser)]
+enum Commands {
+    Codegen {
+        #[clap(long)]
+        check: bool,
+    },
+    Ci {
+        #[clap(long)]
+        fast: bool,
+        toolchain: Option<Toolchain>,
+    },
+    TodomvcRun,
+    TodomvcCypress,
+    #[clap(flatten)]
+    Common(CommonCmds),
+}
+
+fn main() {
+    run(|| {
+        match Commands::parse() {
+            Commands::Codegen { check } => {
+                build_readme(".", check)?;
+                generate_open_source_files(2021, check)?;
+            }
+            Commands::Ci { fast, toolchain } => {
+                build_readme(".", true)?;
+                generate_open_source_files(2021, true)?;
+                ci(fast, toolchain)?;
+
+                if !fast {
+                    cypress("ci")?;
+                }
+
+                if toolchain.map_or(true, |tc| tc == Toolchain::Stable) {
+                    wasm_pack_test()?;
+                }
+            }
+            Commands::TodomvcRun => {
+                let _dir = pushd("examples/todomvc")?;
+                cmd!("trunk serve --open").run()?;
+            }
+            Commands::TodomvcCypress => {
+                cypress("install")?;
+            }
+            Commands::Common(cmds) => cmds.run::<Commands>()?,
+        }
+
+        Ok(())
+    });
+}
+
+fn cypress(install_cmd: &str) -> WorkflowResult<()> {
+    let _dir = pushd("examples/todomvc/e2e")?;
+    cmd!("npm {install_cmd}").run()?;
+    cmd!("npm test").run()?;
+    Ok(())
+}
+
+fn wasm_pack_test() -> WorkflowResult<()> {
+    let _dir = pushd("crates/silkenweb")?;
+    cmd!("wasm-pack test --headless --firefox").run()?;
+    Ok(())
+}
