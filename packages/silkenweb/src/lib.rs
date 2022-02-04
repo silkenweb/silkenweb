c7898441ed40d5b7356e2b25f56dca5035e3cc83
Simon Bourne <simonbourne@gmail.com>
2022-02-04 21:15:10 +0000

    Use intra doc links

--- packages/silkenweb/src/lib.rs
+++ packages/silkenweb/src/lib.rs
@@ -19,10 +19,7 @@
 //! It's not the most complex app, but it'll serve as a good example to show how
 //! we can build an interactive web app. Lets go through the code, step by step.
 //!
-//! Firstly, we create a new `Mutable` and an
-//! associated `Signal`. `Mutable` represents a variable, and `Signal`
-//! represents values of that variable across time. Here we `map` a function
-//! over values of `count`, to convert it to text.
+//! Firstly, we create a new [`Mutable`] and an associated [`Signal`].
 //!
 //! ```rust
 //! # use futures_signals::signal::{Mutable, SignalExt};
@@ -32,8 +29,10 @@
 //! let count_text = count.signal().map(|i| format!("{}", i));
 //! ```
 //!
-//! See the [futures-signals tutorial] for more detail on `Mutable`s and
-//! `Signal`s.
+//! [`Mutable`] represents a variable, and [`Signal`] represents values of that
+//! variable across time. Here we `map` a function over values of `count`, to
+//! convert it to text. See the [futures-signals tutorial] for more detail on
+//! [`Mutable`]s and [`Signal`]s.
 //!
 //! Next, we create a closure, `inc`, to increment `count`. Then we build the
 //! DOM for our counter. `on_click` installs `inc` as an event handler to
@@ -55,8 +54,8 @@
 //!     .child(p().text_signal(count_text));
 //! ```
 //!
-//! Finally, we `mount` our app on the DOM. This will find the element with `id
-//! = "app"` and mount `app` there.
+//! Finally, we [`mount`] our app on the DOM. This will find the element with
+//! `id = "app"` and mount `app` there.
 //!
 //! ```no_run
 //! # use futures_signals::signal::{Mutable, SignalExt};
@@ -88,6 +87,9 @@
 //! [trunk]: https://trunkrs.dev/
 //! [futures-signals tutorial]: https://docs.rs/futures-signals/0.3.24/futures_signals/tutorial/index.html
 //! [counter]: https://github.com/silkenweb/silkenweb/tree/main/examples/counter
+//! [`Mutable`]: futures_signals::signal::Mutable
+//! [`Signal`]: futures_signals::signal::Signal
+//! [`mount`]: crate::dom::mount
 #[doc(inline)]
 pub use silkenweb_dom as dom;
 #[doc(inline)]
