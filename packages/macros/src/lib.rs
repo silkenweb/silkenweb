3e3c30a40a8c1bdc3c7ad47f822f50bc111e86ac
Simon Bourne <simonbourne@gmail.com>
2022-03-01 14:13:21 +0000

    Optionally filter css classes by prefix

--- packages/macros/src/lib.rs
+++ packages/macros/src/lib.rs
@@ -2,34 +2,109 @@ use std::{env, path::PathBuf};
 
 use proc_macro::TokenStream;
 use proc_macro2::Span;
-use proc_macro_error::{abort_call_site, proc_macro_error};
+use proc_macro_error::{abort, abort_call_site, proc_macro_error};
 use quote::quote;
 use syn::{
-    parse::{Parse, ParseStream},
-    parse_macro_input, token, Ident, LitStr, Visibility,
+    parse::{Lookahead1, Parse, ParseStream, Peek},
+    parse_macro_input,
+    token::{Colon, Comma, CustomToken},
+    Ident, LitStr, Visibility,
 };
 
 mod parser;
 
+mod kw {
+    use syn::custom_keyword;
+
+    custom_keyword!(path);
+    custom_keyword!(visibility);
+    custom_keyword!(prefix);
+}
+
 struct Input {
+    path: String,
     visibility: Option<Visibility>,
-    css_file: String,
+    prefix: Option<String>,
+}
+
+impl Input {
+    fn parameter<Keyword, KeywordToken, T>(
+        keyword: Keyword,
+        lookahead: &Lookahead1,
+        input: ParseStream,
+        exists: bool,
+    ) -> syn::Result<bool>
+    where
+        Keyword: Peek + FnOnce(T) -> KeywordToken,
+        KeywordToken: Parse + CustomToken,
+    {
+        Ok(if lookahead.peek(keyword) {
+            if exists {
+                abort!(
+                    input.span(),
+                    "{} is defined multiple times",
+                    KeywordToken::display()
+                );
+            }
+
+            input.parse::<KeywordToken>()?;
+            input.parse::<Colon>()?;
+
+            true
+        } else {
+            false
+        })
+    }
 }
 
 impl Parse for Input {
     fn parse(input: ParseStream) -> syn::Result<Self> {
-        let visibility = if input.peek(token::Pub) {
-            Some(input.parse()?)
+        if input.peek(LitStr) {
+            return Ok(Self {
+                path: input.parse::<LitStr>()?.value(),
+                visibility: None,
+                prefix: None,
+            });
+        }
+
+        let mut path = None;
+        let mut visibility = None;
+        let mut prefix = None;
+        let mut trailing_comma = true;
+
+        while !input.is_empty() {
+            if !trailing_comma {
+                abort!(input.span(), "Expected ','");
+            }
+
+            let lookahead = input.lookahead1();
+
+            if Self::parameter(kw::path, &lookahead, input, path.is_some())? {
+                path = Some(input.parse::<LitStr>()?.value());
+            } else if Self::parameter(kw::visibility, &lookahead, input, visibility.is_some())? {
+                visibility = Some(input.parse()?);
+            } else if Self::parameter(kw::prefix, &lookahead, input, prefix.is_some())? {
+                prefix = Some(input.parse::<LitStr>()?.value());
+            } else {
+                return Err(lookahead.error());
+            }
+
+            trailing_comma = input.peek(Comma);
+
+            if trailing_comma {
+                input.parse::<Comma>()?;
+            }
+        }
+
+        if let Some(path) = path {
+            Ok(Self {
+                visibility,
+                path,
+                prefix,
+            })
         } else {
-            None
-        };
-
-        let css_file = input.parse::<LitStr>()?.value();
-
-        Ok(Self {
-            visibility,
-            css_file,
-        })
+            abort_call_site!("Missing 'path' parameter");
+        }
     }
 }
 
@@ -41,7 +116,9 @@ impl Parse for Input {
 /// The input file is relative to the `$CARGO_MANIFEST_DIR` environment
 /// variable.
 ///
-/// # Example
+/// # Examples
+///
+/// Define private constants for all CSS classes:
 ///
 ///  ```
 /// # use silkenweb_macros::css_classes;
@@ -49,54 +126,87 @@ impl Parse for Input {
 /// assert_eq!(MY_CLASS, "my-class");
 /// ```
 /// 
-/// An optional visibility modifier can be specified:
+/// Optional `visibility` and `prefix` parameters can be specified. `visibility` is any visibility
+/// modifier and controls the visibility of class constants. If `prefix` is specified, only classes
+/// starting with `prefix` will have constants defined, and their Rust names will have the prefix
+/// stripped.
 /// ```
-/// mod css {
+/// mod border {
 ///     # use silkenweb_macros::css_classes;
-///     css_classes!(pub "my-sass-file.scss");
+///     css_classes!(
+///         visibility: pub,
+///         path: "my-sass-file.scss",
+///         prefix:"border-"
+///     );
 /// }
 ///
-/// assert_eq!(css::MY_CLASS, "my-class");
+/// assert_eq!(border::SMALL, "border-small");
 /// ```
-///
 #[proc_macro]
 #[proc_macro_error]
 pub fn css_classes(input: TokenStream) -> TokenStream {
     let Input {
         visibility,
-        css_file,
+        path,
+        prefix,
     } = parse_macro_input!(input);
 
     let root_dir = env::var("CARGO_MANIFEST_DIR")
         .unwrap_or_else(|_| abort_call_site!("Unable to read {}", CARGO_MANIFEST_DIR));
-    let css_file = PathBuf::from(root_dir)
-        .join(css_file)
+    let path = PathBuf::from(root_dir)
+        .join(path)
         .into_os_string()
         .into_string()
         .expect("Expected path to be convertible to string");
 
-    let classes = parser::class_names(&css_file)
-        .unwrap_or_else(|e| abort_call_site!("'{}': {}", css_file, e.to_string()));
+    let classes = parser::class_names(&path)
+        .unwrap_or_else(|e| abort_call_site!("'{}': {}", path, e.to_string()));
 
-    let classes = classes.map(|class| {
-        if !class.starts_with(char::is_alphabetic) {
+    if let Some(prefix) = prefix {
+        code_gen(
+            visibility,
+            &path,
+            classes.filter_map(|class| {
+                let class_ident = class.strip_prefix(&prefix).map(str::to_string);
+                class_ident.map(|class_ident| {
+                    println!("{}, {}", class_ident, class);
+                    (class_ident, class)
+                })
+            }),
+        )
+    } else {
+        code_gen(
+            visibility,
+            &path,
+            classes.map(|class| (class.clone(), class)),
+        )
+    }
+}
+
+fn code_gen(
+    visibility: Option<Visibility>,
+    path: &str,
+    classes: impl Iterator<Item = (String, String)>,
+) -> TokenStream {
+    let classes = classes.map(|(class_ident, class_name)| {
+        if !class_ident.starts_with(char::is_alphabetic) {
             abort_call_site!(
                 "Identifier '{}' doesn't start with an alphabetic character",
-                class
+                class_ident
             );
         }
 
         let class_ident = Ident::new(
-            &class
+            &class_ident
                 .replace(|c: char| !c.is_alphanumeric(), "_")
                 .to_uppercase(),
             Span::call_site(),
         );
-        quote!(#visibility const #class_ident: &str = #class;)
+        quote!(#visibility const #class_ident: &str = #class_name;)
     });
 
     quote!(
-        const _: &[u8] = ::std::include_bytes!(#css_file);
+        const _: &[u8] = ::std::include_bytes!(#path);
         #(#classes)*
     )
     .into()
