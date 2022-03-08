a82a21bc699b114083faf5d9f424ce8872607047
Simon Bourne <simonbourne@gmail.com>
2022-03-08 23:49:48 +0000

    Organize imports

--- examples/ui5-showcase/ui5-bundler/main.js
+++ examples/ui5-showcase/ui5-bundler/main.js
@@ -1,12 +1,11 @@
 import "@ui5/webcomponents/dist/Badge";
 import "@ui5/webcomponents/dist/Button";
+import "@ui5/webcomponents/dist/Calendar";
+import "@ui5/webcomponents/dist/Icon";
 
 import "@ui5/webcomponents-fiori/dist/SideNavigation";
 import "@ui5/webcomponents-fiori/dist/SideNavigationItem";
 import "@ui5/webcomponents-fiori/dist/SideNavigationSubItem";
 
-import "@ui5/webcomponents/dist/Icon";
 import "@ui5/webcomponents-icons/dist/activate";
 import "@ui5/webcomponents-icons/dist/accept";
-
-import "@ui5/webcomponents/dist/Calendar";
