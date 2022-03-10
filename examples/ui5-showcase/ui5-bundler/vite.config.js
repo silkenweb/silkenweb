7d228af865f58e2623a61dc00808e3ee54238c2a
Simon Bourne <simonbourne@gmail.com>
2022-03-10 22:27:01 +0000

    Minify UI5 JS

--- examples/ui5-showcase/ui5-bundler/vite.config.js
+++ examples/ui5-showcase/ui5-bundler/vite.config.js
@@ -2,7 +2,17 @@ const path = require('path')
 const { defineConfig } = require('vite')
 
 module.exports = defineConfig({
+    // TODO: This is a workaround to enable minification for ES builds.
+    // See:
+    // - <https://github.com/vitejs/vite/issues/6555>
+    // - <https://github.com/vitejs/vite/pull/6670>
+    esbuild: {
+        minify: true,
+    },
     build: {
+        // TODO: One of the proposed solutions to minifying ES builds is to
+        // default `minify`to false, then allow us to opt-in.
+        minify: true,
         lib: {
             entry: path.resolve(__dirname, 'main.js'),
             name: 'UI5',
