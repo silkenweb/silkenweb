97febdcef07849d0b6f3c86f28f6dc0a924bd933
Simon Bourne <simonbourne@gmail.com>
2022-10-02 23:33:46 +0100

    Tidy routing example

--- examples/router/src/main.rs
+++ examples/router/src/main.rs
@@ -1,46 +1,42 @@
 use silkenweb::{
     elements::{
-        html::{button, div, p},
+        html::{button, div, p, Button},
         ElementEvents,
     },
     mount,
+    node::element::ElementBuilder,
     prelude::ParentBuilder,
-    router,
+    router::{self, UrlPath},
 };
 
 fn main() {
     mount("app", {
         div()
-            .child(
-                button()
-                    .on_click(|_, _| router::set_url_path("/basic"))
-                    .text("Go to basic route"),
-            )
-            .child(
-                button()
-                    .on_click(|_, _| router::set_url_path("/with_args/arg1/arg2"))
-                    .text("Go to route with args"),
-            )
-            .child(
-                button()
-                    .on_click(|_, _| router::set_url_path("/with_query?x=1&y=2"))
-                    .text("Go to route with query string"),
-            )
+            .child(route_button("", "Root route"))
+            .child(route_button("basic", "Basic route"))
+            .child(route_button("with_args/arg1/arg2", "Go to route with args"))
+            .child(route_button(
+                "with_query?x=1&y=2&flag",
+                "Go to route with query string",
+            ))
             .child(p().text_signal(router::url_path().signal_ref(|url_path| {
                 let path_components = url_path.path_components_vec();
 
                 match &path_components[..] {
-                    [] => "This is the root route!".to_string(),
-                    ["basic"] => "This is a basic route".to_string(),
-                    ["with_args", args @ ..] => format!("This is a route with args {args:?}"),
-                    ["with_query"] => {
-                        format!(
-                            "This is a route with a query string: {:?}",
-                            url_path.query().collect::<Vec<_>>()
-                        )
-                    }
+                    [] => "Root route!".to_string(),
+                    ["basic"] => "Basic route".to_string(),
+                    ["with_args", args @ ..] => format!("Route with args {args:?}"),
+                    ["with_query"] => format!("Route with query: {:?}", url_path.query_map()),
                     _ => "Unknown route".to_string(),
                 }
             })))
     });
+}
+
+fn route_button(route: &str, description: &str) -> Button {
+    let route = UrlPath::new(route);
+    button()
+        .on_click(move |_, _| router::set_url_path(route.clone()))
+        .text(description)
+        .build()
 }
