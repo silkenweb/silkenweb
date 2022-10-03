5a34940d9ad78c64353e4e8bfa18059fba139711
Simon Bourne <simonbourne@gmail.com>
2022-10-03 16:54:53 +0100

    Add `SVG `a` element.

--- packages/silkenweb/src/elements/svg.rs
+++ packages/silkenweb/src/elements/svg.rs
@@ -9,6 +9,56 @@ pub mod attributes;
 pub mod content_type;
 pub mod path;
 
+svg_element!(
+    a <web_sys::SvgaElement> {
+        attributes {
+            /// Instructs browsers to download a URL instead of navigating to
+            /// it, so the user will be prompted to save it as a local file.
+            /// Value type: <string> ; Default value: none; Animatable: no
+            download: String,
+
+            /// The URL or URL fragment the hyperlink points to.
+            /// Value type: <URL> ;
+            /// Default value: none; Animatable: yes
+            href: String,
+
+            /// The human language of the URL or URL fragment that the hyperlink
+            /// points to.
+            /// Value type: <string> ;
+            /// Default value: none; Animatable: yes
+            hreflang: String,
+
+            /// A space-separated list of URLs to which, when the hyperlink is
+            /// followed, POST requests with the body PING will be sent by the
+            /// browser (in the background). Typically used for tracking. For a
+            /// more widely-supported feature addressing the same use cases, see
+            /// Navigator.sendBeacon().
+            /// Value type: <list-of-URLs> ;
+            /// Default value: none; Animatable: no
+            ping: String,
+
+            /// Which referrer to send when fetching the URL.
+            /// Value type: no-referrer|no-referrer-when-downgrade|same-origin|origin|strict-origin|origin-when-cross-origin|strict-origin-when-cross-origin|unsafe-url ;
+            /// Default value: none; Animatable: no
+            referrerpolicy: String,
+
+            /// The relationship of the target object to the link object.
+            /// Value type: <list-of-Link-Types> ;
+            /// Default value: none; Animatable: yes
+            rel: String,
+
+            /// Where to display the linked URL.
+            /// Value type: _self|_parent|_top|_blank|<name> ;
+            /// Default value: _self; Animatable: yes
+            target: String,
+
+            /// A MIME type for the linked URL.
+            /// Value type: <string> ; Default value: none; Animatable: yes
+            r#type: String,
+        }
+    }
+);
+
 // TODO: Add all svg elements, (element, global) * (attributes, events)
 svg_element!(
     svg <web_sys::SvgsvgElement> {
