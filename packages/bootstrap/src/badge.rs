5ad41d38f56bde2917b598755b7fa012dd12f178
Simon Bourne <simonbourne@gmail.com>
2022-10-14 15:32:49 +0100

    Use `extend_html_element`

--- packages/bootstrap/src/badge.rs
+++ packages/bootstrap/src/badge.rs
@@ -1,6 +1,7 @@
-use futures_signals::signal::Signal;
+use futures_signals::signal::{Signal, SignalExt};
 use silkenweb::{
     elements::html::{span, SpanBuilder},
+    extend_html_element,
     node::{
         element::{Element, ElementBuilder},
         Node,
@@ -16,44 +17,30 @@ pub struct Badge(SpanBuilder);
 
 impl Badge {
     pub fn new(text: &str, background_colour: Colour) -> Self {
-        Self(Self::base_element(background_colour).text(text))
+        Self(
+            span()
+                .class([css::BADGE, background_colour.text_background_class()])
+                .text(text),
+        )
     }
 
     pub fn new_signal(
         text: impl Signal<Item = impl Into<String>> + 'static,
-        background_colour: Colour,
+        background_colour: impl Signal<Item = Colour> + 'static,
     ) -> Self {
-        Self(Self::base_element(background_colour).text_signal(text))
+        Self(
+            span()
+                // TODO: `class [_signal]` should be renamed to `classes [_signal]` and `class
+                // [_signal]` should take a single class
+                .class([css::BADGE])
+                .class_signal(background_colour.map(|colour| [colour.text_background_class()]))
+                .text_signal(text),
+        )
     }
 
     pub fn rounded_pill(self) -> Self {
         Self(self.0.class([css::ROUNDED_PILL]))
     }
-
-    fn base_element(background_colour: Colour) -> SpanBuilder {
-        span().class([css::BADGE, background_colour.text_background_class()])
-    }
 }
 
-impl HtmlElement for Badge {}
-impl HtmlElementEvents for Badge {}
-impl ElementEvents for Badge {}
-
-// TODO: Should this be part of derive(ElementBuilder)?
-impl From<Badge> for Element {
-    fn from(badge: Badge) -> Self {
-        badge.0.into()
-    }
-}
-
-impl From<Badge> for Node {
-    fn from(badge: Badge) -> Self {
-        badge.0.into()
-    }
-}
-
-impl From<Badge> for SpanBuilder {
-    fn from(badge: Badge) -> Self {
-        badge.0
-    }
-}
+extend_html_element!(Badge {0});
