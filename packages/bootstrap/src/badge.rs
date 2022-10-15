da45ff952043ab067d7923972886659d92ab69a6
Simon Bourne <simonbourne@gmail.com>
2022-10-15 11:49:36 +0100

    Support bootstrap link badges

--- packages/bootstrap/src/badge.rs
+++ packages/bootstrap/src/badge.rs
@@ -1,41 +1,62 @@
-use derive_more::{From, Into};
+use derive_more::Into;
 use futures_signals::signal::{Signal, SignalExt};
 use silkenweb::{
-    elements::html::{span, Span, SpanBuilder},
+    elements::html::{span, ABuilder},
     node::{element::ElementBuilder, Node},
-    prelude::ParentBuilder,
+    prelude::{ElementEvents, HtmlElementEvents, ParentBuilder},
     ElementBuilder,
 };
 
 use crate::{
     css,
     utility::{Colour, SetBorder},
+    HtmlElementBuilder,
 };
 
 #[derive(ElementBuilder, Into)]
 #[element_target(Badge)]
-pub struct BadgeBuilder(SpanBuilder);
+pub struct BadgeBuilder(HtmlElementBuilder);
 
 pub fn badge(text: &str, background_colour: Colour) -> BadgeBuilder {
-    BadgeBuilder(
+    BadgeBuilder(HtmlElementBuilder(
         span()
             .class([css::BADGE, background_colour.text_background()])
-            .text(text),
-    )
+            .text(text)
+            .into(),
+    ))
 }
 
 pub fn badge_signal(
     text: impl Signal<Item = impl Into<String>> + 'static,
     background_colour: impl Signal<Item = Colour> + 'static,
 ) -> BadgeBuilder {
-    BadgeBuilder(
+    BadgeBuilder(HtmlElementBuilder(
         span()
             // TODO: `class [_signal]` should be renamed to `classes [_signal]` and `class
             // [_signal]` should take a single class
             .class([css::BADGE])
             .class_signal(background_colour.map(|colour| [colour.text_background()]))
-            .text_signal(text),
-    )
+            .text_signal(text)
+            .into(),
+    ))
+}
+
+pub fn link_badge(link: ABuilder, background_colour: Colour) -> BadgeBuilder {
+    BadgeBuilder(HtmlElementBuilder(
+        link.class([css::BADGE, background_colour.text_background()])
+            .into(),
+    ))
+}
+
+pub fn link_badge_signal(
+    link: ABuilder,
+    background_colour: impl Signal<Item = Colour> + 'static,
+) -> BadgeBuilder {
+    BadgeBuilder(HtmlElementBuilder(
+        link.class([css::BADGE])
+            .class_signal(background_colour.map(|colour| [colour.text_background()]))
+            .into(),
+    ))
 }
 
 impl BadgeBuilder {
@@ -44,8 +65,8 @@ impl BadgeBuilder {
     }
 }
 
-#[derive(From)]
-pub struct Badge(Span);
+impl ElementEvents for BadgeBuilder {}
+impl HtmlElementEvents for BadgeBuilder {}
 
 impl From<BadgeBuilder> for Node {
     fn from(badge: BadgeBuilder) -> Self {
@@ -53,8 +74,11 @@ impl From<BadgeBuilder> for Node {
     }
 }
 
-impl From<Badge> for Node {
-    fn from(badge: Badge) -> Self {
-        badge.0.into()
+#[derive(Into)]
+pub struct Badge(Node);
+
+impl From<HtmlElementBuilder> for Badge {
+    fn from(builder: HtmlElementBuilder) -> Self {
+        Self(builder.0.into())
     }
 }
--- packages/bootstrap/src/lib.rs
+++ packages/bootstrap/src/lib.rs
@@ -1,9 +1,13 @@
+use silkenweb::{
+    node::{element::ElementBuilderBase, Node},
+    prelude::HtmlElement,
+    ElementBuilder,
+};
+
 pub mod badge;
 pub mod utility;
 
 pub mod css {
-    // TODO: Should `css_classes!` be able to make a trait to apply the class to an
-    // `ElementBuilder`?
     silkenweb::css_classes!(visibility: pub, path: "bootstrap-5.2.2/css/bootstrap.min.css");
 }
 
@@ -12,3 +16,18 @@ pub mod icon {
 }
 
 pub type Class = &'static str;
+
+/// A generic HTML element builder
+///
+/// Some bootstrap types, like [`Badge`](badge::Badge) will convert into this as
+/// an "escape hatch".
+#[derive(ElementBuilder)]
+pub struct HtmlElementBuilder(ElementBuilderBase);
+
+impl HtmlElement for HtmlElementBuilder {}
+
+impl From<HtmlElementBuilder> for Node {
+    fn from(builder: HtmlElementBuilder) -> Self {
+        builder.0.into()
+    }
+}
