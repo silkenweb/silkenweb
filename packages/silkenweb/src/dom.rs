a358c239226b2a5232b75ea46f1e1abe9a879a94
Simon Bourne <simonbourne@gmail.com>
2022-12-09 14:47:42 +0000

    Add some more methods to DomType

--- packages/silkenweb/src/dom.rs
+++ packages/silkenweb/src/dom.rs
@@ -1,4 +1,6 @@
-use crate::hydration::node::Namespace;
+use wasm_bindgen::JsValue;
+
+use crate::{attribute::Attribute, hydration::node::Namespace};
 
 pub mod wet;
 
@@ -8,9 +10,28 @@ pub trait Dom {
     type Node;
 }
 
-// TODO: Is `Clone` required?
 pub trait DomElement: Clone {
     fn new(ns: Namespace, tag: &str) -> Self;
+
+    fn append_child(&mut self, child: Self);
+
+    fn insert_child_before(&mut self, child: Self, next_child: Option<Self>);
+
+    fn replace_child(&mut self, new_child: Self, old_child: Self);
+
+    fn remove_child(&mut self, child: Self);
+
+    fn clear_children(&mut self);
+
+    fn add_class(&mut self, name: &str);
+
+    fn remove_class(&mut self, name: &str);
+
+    fn attribute<A>(&mut self, name: &str, value: A)
+    where
+        A: Attribute;
+
+    fn on(&mut self, name: &'static str, f: impl FnMut(JsValue) + 'static);
 }
 
 pub type DefaultDom = wet::Wet;
--- packages/silkenweb/src/dom/wet.rs
+++ packages/silkenweb/src/dom/wet.rs
@@ -1,3 +1,6 @@
+use silkenweb_base::{document, intern_str};
+use wasm_bindgen::{prelude::Closure, JsCast, JsValue, UnwrapThrowExt};
+
 use super::{Dom, DomElement};
 use crate::hydration::node::Namespace;
 
@@ -9,18 +12,74 @@ impl Dom for Wet {
     type Text = WetText;
 }
 
-pub struct WetElement {}
-
-impl DomElement for WetElement {
-    fn new(_ns: Namespace, _tag: &str) -> Self {
-        WetElement {}
-    }
+#[derive(Clone)]
+pub struct WetElement {
+    element: web_sys::Element,
 }
 
-// TODO: Is `Clone` required?
-impl Clone for WetElement {
-    fn clone(&self) -> Self {
-        Self {}
+impl DomElement for WetElement {
+    fn new(ns: Namespace, tag: &str) -> Self {
+        let element = match ns {
+            Namespace::Html => document::create_element(tag),
+            Namespace::Other(ns) => document::create_element_ns(ns.map(intern_str), tag),
+        };
+
+        Self { element }
+    }
+
+    fn append_child(&mut self, child: Self) {
+        self.element.append_child(&child.element).unwrap_throw();
+    }
+
+    fn insert_child_before(&mut self, child: Self, next_child: Option<Self>) {
+        self.element
+            .insert_before(
+                &child.element,
+                next_child.map(|c| c.element.into()).as_ref(),
+            )
+            .unwrap_throw();
+    }
+
+    fn replace_child(&mut self, new_child: Self, old_child: Self) {
+        self.element
+            .replace_child(&new_child.element, &old_child.element)
+            .unwrap_throw();
+    }
+
+    fn remove_child(&mut self, child: Self) {
+        self.element.remove_child(&child.element).unwrap_throw();
+    }
+
+    fn clear_children(&mut self) {
+        self.element.set_inner_html("")
+    }
+
+    fn add_class(&mut self, name: &str) {
+        self.element.class_list().add_1(name).unwrap_throw()
+    }
+
+    fn remove_class(&mut self, name: &str) {
+        self.element.class_list().remove_1(name).unwrap_throw()
+    }
+
+    fn attribute<A>(&mut self, name: &str, value: A)
+    where
+        A: crate::attribute::Attribute,
+    {
+        if let Some(attr) = value.text() {
+            self.element.set_attribute(name, &attr)
+        } else {
+            self.element.remove_attribute(name)
+        }
+        .unwrap_throw()
+    }
+
+    fn on(&mut self, name: &'static str, f: impl FnMut(JsValue) + 'static) {
+        // TODO: This only works with weak-refs. We need to store the callback for
+        // none-weak-refs
+        self.element
+            .add_event_listener_with_callback(name, Closure::new(f).into_js_value().unchecked_ref())
+            .unwrap_throw();
     }
 }
 
