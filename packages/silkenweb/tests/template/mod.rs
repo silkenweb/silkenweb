5b8d952c6cf1fc48c4ba22adda9eae60a953c681
Simon Bourne <simonbourne@gmail.com>
2022-12-21 09:35:43 +0000

    Add `dry_clone_node_is_deep` test

--- packages/silkenweb/tests/template/mod.rs
+++ packages/silkenweb/tests/template/mod.rs
@@ -9,8 +9,8 @@ use silkenweb::{
     },
     node::Node,
     prelude::ParentElement,
-    task::render_now,
-    value::Sig,
+    task::{render_now, server::render_now_sync},
+    value::Sig, dom::Dry,
 };
 
 use super::PlatformDom;
@@ -87,4 +87,20 @@ isomorphic_test! {
         children.lock_mut().clear();
         check(&template, (), r#"<div></div>"#).await;
     }
+}
+
+#[test]
+fn dry_clone_node_is_deep() {
+    let template: DivTemplate<String, Dry> = div().child(div().on_instantiate(|div, s| div.text(s))).freeze();
+    render_now_sync();
+    let node1: Node<Dry> = template.instantiate(&"Hello, world!".to_string()).into();
+    let expected_node1 = r#"<div><div>Hello, world!</div></div>"#;
+    assert_eq!(node1.to_string(), expected_node1);
+
+    render_now_sync();
+    let node2: Node<Dry> = template.instantiate(&"Goodbye!".to_string()).into();
+    assert_eq!(node2.to_string(), r#"<div><div>Goodbye!</div></div>"#);
+
+    // If the dry clone isn't deep, this will fail.
+    assert_eq!(node1.to_string(), expected_node1);
 }
