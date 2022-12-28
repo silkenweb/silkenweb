2e058e06253cea73c62aff53781b22f6738e25e9
Simon Bourne <simonbourne@gmail.com>
2022-12-28 15:38:54 +0000

    Simplify UI5 bundler

--- examples/ui5-showcase/ui5-bundler/vite.config.js
+++ examples/ui5-showcase/ui5-bundler/vite.config.js
@@ -1,25 +1,12 @@
-const path = require('path')
-const { defineConfig } = require('vite')
+import { resolve } from 'path';
+import { defineConfig } from 'vite';
 
 module.exports = defineConfig({
-    // TODO: This is a workaround to enable minification for ES builds.
-    //
-    // One of the proposed solutions to minifying ES builds is to default
-    // `build.minify`to false, then allow us to opt-in.
-    //
-    // See:
-    // - <https://github.com/vitejs/vite/issues/6555>
-    // - <https://github.com/vitejs/vite/pull/6670>
-    esbuild: {
-        minify: true,
-    },
     build: {
-        minify: true,
         lib: {
-            entry: path.resolve(__dirname, 'main.js'),
+            entry: resolve(__dirname, 'main.js'),
             name: 'UI5',
-            fileName: (format) => `ui5.${format}.js`,
-            formats: ['esm'],
+            formats: ['iife'],
         },
-    }
+    },
 })
