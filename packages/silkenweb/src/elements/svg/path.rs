6d418cd6dc1d412e286c0302557eb153cd3d8619
Simon Bourne <simonbourne@gmail.com>
2023-01-05 19:16:59 +0000

    Docs for `svg::path`

--- packages/silkenweb/src/elements/svg/path.rs
+++ packages/silkenweb/src/elements/svg/path.rs
@@ -1,15 +1,39 @@
+//! Tools to construct an SVG path.
 use std::fmt::Write;
 
 use silkenweb_signals_ext::value::Value;
 
 use crate::attribute::{AsAttribute, Attribute};
 
+/// Absolute or relative indicator for a path component.
 #[derive(Copy, Clone)]
 pub enum Offset {
     Abs,
     Rel,
 }
 
+/// Construct path data for the [`d`] attribute
+///
+/// # Example
+///
+/// ```
+/// # use silkenweb::{dom::Dry, prelude::*};
+/// # use svg::{
+/// #     attributes::Presentation,
+/// #     path,
+/// #     path::{Data, Offset::Abs},
+/// #     Path,
+/// # };
+/// let path: Path<Dry> = path().d(Data::new()
+///     .move_to(Abs, 10.0, 10.0)
+///     .lines_to(Abs, [(20.0, 20.0), (30.0, 30.0)]));
+///
+/// assert_eq!(
+///     r#"<path d="M 10,10 L 20,20 30,30"></path>"#,
+///     path.freeze().to_string()
+/// );
+/// ```
+/// [`d`]: `super::attributes::Presentation::d`
 #[derive(Default, Clone)]
 pub struct Data(String);
 
@@ -22,22 +46,37 @@ impl Data {
         Self(String::with_capacity(capacity))
     }
 
+    /// The `m/M` commands ([MDN Docs])
+    ///
+    /// [MDN Docs]: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d#moveto_path_commands
     pub fn move_to(self, offset: Offset, x: f64, y: f64) -> Self {
         self.cmd2(offset, 'm', [(x, y)])
     }
 
+    /// The `l/L` commands ([MDN Docs])
+    ///
+    /// [MDN Docs]: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d#lineto_path_commands
     pub fn lines_to(self, offset: Offset, ends: impl IntoIterator<Item = (f64, f64)>) -> Self {
         self.cmd2(offset, 'l', ends)
     }
 
+    /// The `h/H` commands ([MDN Docs])
+    ///
+    /// [MDN Docs]: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d#lineto_path_commands
     pub fn horizontal_lines_to(self, offset: Offset, ends: impl IntoIterator<Item = f64>) -> Self {
         self.cmd1(offset, 'h', ends)
     }
 
+    /// The `v/V` commands ([MDN Docs])
+    ///
+    /// [MDN Docs]: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d#lineto_path_commands
     pub fn vertical_lines_to(self, offset: Offset, ends: impl IntoIterator<Item = f64>) -> Self {
         self.cmd1(offset, 'v', ends)
     }
 
+    /// The `c/C` commands ([MDN Docs])
+    ///
+    /// [MDN Docs]: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d#cubic_b%C3%A9zier_curve
     pub fn cubic_bezier_curves(
         self,
         offset: Offset,
@@ -46,6 +85,9 @@ impl Data {
         self.cmd6(offset, 'c', args)
     }
 
+    /// The `s/S` commands ([MDN Docs])
+    ///
+    /// [MDN Docs]: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d#cubic_b%C3%A9zier_curve
     pub fn smooth_cubic_bezier_curves(
         self,
         offset: Offset,
@@ -54,6 +96,9 @@ impl Data {
         self.cmd4(offset, 's', args)
     }
 
+    /// The `q/Q` commands ([MDN Docs])
+    ///
+    /// [MDN Docs]: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d#quadratic_b%C3%A9zier_curve
     pub fn quadradic_bezier_curves(
         self,
         offset: Offset,
@@ -62,6 +107,9 @@ impl Data {
         self.cmd4(offset, 'q', args)
     }
 
+    /// The `t/T` commands ([MDN Docs])
+    ///
+    /// [MDN Docs]: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d#quadratic_b%C3%A9zier_curve
     pub fn smooth_quadradic_bezier_curves(
         self,
         offset: Offset,
@@ -70,6 +118,9 @@ impl Data {
         self.cmd2(offset, 't', end_points)
     }
 
+    /// The `a/A` commands ([MDN Docs])
+    ///
+    /// [MDN Docs]: https://developer.mozilla.org/en-US/docs/Web/SVG/Attribute/d#elliptical_arc_curve
     pub fn elliptical_arc_curves(
         self,
         offset: Offset,
