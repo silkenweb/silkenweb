9beab40876a458eed8692088b6d21bcdba98822c
Simon Bourne <simonbourne@gmail.com>
2023-01-16 23:57:51 +0000

    Improve CSS modules example

--- examples/css-modules/src/main.rs
+++ examples/css-modules/src/main.rs
@@ -1,8 +1,29 @@
-use silkenweb::{css, elements::html::*, prelude::*};
+use silkenweb::{elements::html::*, prelude::*};
 
-css!(path = "stylesheet.css", auto_mount, transpile = (modules));
+mod red {
+    silkenweb::css!(path = "red.css", auto_mount, transpile = (modules));
+
+    // TODO: What if we wanted to export the `mod class` as `pub`?
+    // modules = {
+    // pub use classes as my_mod;
+    // pub use stylesheet as my_mod2;
+    // },
+
+    pub use class::*;
+}
+
+mod green {
+    silkenweb::css!(path = "green.css", auto_mount, transpile = (modules));
+
+    pub use class::*;
+}
 
 fn main() {
-    let app = div().class(class::my_class()).text("Hello, world!");
+    // "red.css" and "green.css" use the same class name for text color, but this is
+    // scoped using `modules`.
+    let app = div().children([
+        div().class(red::color()).text("Red text"),
+        div().class(green::color()).text("Green text"),
+    ]);
     mount("app", app);
 }
