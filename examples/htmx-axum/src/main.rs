71b5c6e7154dfd8005f3ca4f16c9877bc0f9d33e
Simon Bourne <simonbourne@gmail.com>
2023-03-11 18:39:39 +0000

    Basic htmx support with example

--- examples/htmx-axum/src/main.rs
+++ examples/htmx-axum/src/main.rs
@@ -1,8 +1,10 @@
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
 
 use axum::{
-    headers::ContentType, response::IntoResponse, routing::get, Router, Server, TypedHeader,
+    headers::ContentType, response::IntoResponse, routing::{get, post}, Router, Server, TypedHeader,
 };
+use silkenweb::prelude::{html::div, ParentElement};
+use silkenweb_htmx_axum::HtmxResponse;
 use tracing::info;
 
 async fn index() -> impl IntoResponse {
@@ -10,6 +12,12 @@ async fn index() -> impl IntoResponse {
         TypedHeader(ContentType::html()),
         include_str!("../index.html"),
     )
+}
+
+// TODO: Read some data from the post request
+async fn button_clicked() -> impl IntoResponse {
+    info!("Button clicked");
+    HtmxResponse::new(div().text("Button clicked"))
 }
 
 #[tokio::main]
@@ -20,7 +28,9 @@ async fn main() {
 
     tracing::subscriber::set_global_default(subscriber).unwrap();
 
-    let app = Router::new().route("/", get(index));
+    let app = Router::new()
+        .route("/", get(index))
+        .route("/button-clicked", post(button_clicked));
 
     let address = SocketAddr::new(IpAddr::V4(Ipv4Addr::LOCALHOST), 9090);
     let server = Server::bind(&address);
--- packages/htmx-axum/src/lib.rs
+++ packages/htmx-axum/src/lib.rs
@@ -4,7 +4,7 @@ use async_trait::async_trait;
 use axum::{
     extract::FromRequest,
     http::{self, Request},
-    response::{IntoResponse, Response},
+    response::{IntoResponse, Response}, TypedHeader, headers::ContentType,
 };
 use serde::de::DeserializeOwned;
 use silkenweb::{dom::Dry, prelude::Node};
@@ -19,7 +19,7 @@ impl HtmxResponse {
 
 impl IntoResponse for HtmxResponse {
     fn into_response(self) -> Response {
-        self.0.to_string().into_response()
+        (TypedHeader(ContentType::html()), self.0.to_string()).into_response()
     }
 }
 
