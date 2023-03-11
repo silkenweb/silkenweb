99fbc8386142f51aee2a9835324d9cbb6de2d90d
Simon Bourne <simonbourne@gmail.com>
2023-03-11 17:57:11 +0000

    Start htmx integration

---
+++ packages/htmx-axum/src/lib.rs
@@ -0,0 +1,43 @@
+use std::io;
+
+use async_trait::async_trait;
+use axum::{
+    extract::FromRequest,
+    http::{self, Request},
+    response::{IntoResponse, Response},
+};
+use serde::de::DeserializeOwned;
+use silkenweb::{dom::Dry, prelude::Node};
+
+pub struct HtmxResponse(Node<Dry>);
+
+impl HtmxResponse {
+    pub fn new(node: impl Into<Node<Dry>>) -> Self {
+        Self(node.into())
+    }
+}
+
+impl IntoResponse for HtmxResponse {
+    fn into_response(self) -> Response {
+        self.0.to_string().into_response()
+    }
+}
+
+pub struct HtmxPostRequest<T>(T);
+
+#[async_trait]
+impl<State, Body, T> FromRequest<State, Body> for HtmxPostRequest<T>
+where
+    State: Send + Sync,
+    Body: Send + 'static,
+    for<'a> &'a Body: io::Read,
+    T: DeserializeOwned,
+{
+    type Rejection = http::StatusCode;
+
+    async fn from_request(req: Request<Body>, _state: &State) -> Result<Self, Self::Rejection> {
+        serde_urlencoded::from_reader(req.body())
+            .map_err(|_| http::StatusCode::BAD_REQUEST)
+            .map(HtmxPostRequest)
+    }
+}
