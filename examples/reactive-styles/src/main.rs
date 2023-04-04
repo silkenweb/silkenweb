5568eaa31d1cccd457e990f2a9050986453b41d2
Simon Bourne <simonbourne@gmail.com>
2023-04-04 16:53:23 +0100

    Add a reactive styles example

---
+++ examples/reactive-styles/src/main.rs
@@ -0,0 +1,28 @@
+use silkenweb::{elements::html::*, prelude::*, stylesheet::StyleDeclaration};
+
+silkenweb::css!("color.css");
+
+fn change_color(
+    color: &Mutable<&'static str>,
+    description: &str,
+    new_color: &'static str,
+) -> Button {
+    clone!(color);
+    button()
+        .text(description)
+        .on_click(move |_, _| color.set(new_color))
+}
+
+fn main() {
+    let color = Mutable::new("green");
+    let app = div()
+        .style_property(StyleDeclaration::new().style("--color", Sig(color.signal_cloned())))
+        .child(change_color(&color, "Red", "red"))
+        .child(change_color(&color, "Green", "green"))
+        .child(
+            div()
+                .class(class::COLOR)
+                .text("Click either the 'Red' or 'Green' button to set the color of this text."),
+        );
+    mount("app", app);
+}
