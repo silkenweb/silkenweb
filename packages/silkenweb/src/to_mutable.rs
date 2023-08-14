2623d56d27ff4ee0cb5c6c74414a0dffd9d97322
Henrik Akesson <henrik.akesson@crionis.com>
2023-08-14 06:54:45 +0200

    Remove Default requirement for .to_mutable

--- packages/silkenweb/src/to_mutable.rs
+++ packages/silkenweb/src/to_mutable.rs
@@ -1,3 +1,5 @@
+use async_trait::async_trait;
+use futures::StreamExt;
 use futures_signals::{
     signal::{Mutable, ReadOnlyMutable, Signal, SignalExt},
     signal_vec::MutableVec,
@@ -5,27 +7,34 @@ use futures_signals::{
 
 use crate::task::spawn_local;
 
-pub trait SignalToMutable<TSig: Default + 'static>: Signal<Item = TSig> + Sized {
-    fn to_mutable(self) -> ReadOnlyMutable<TSig>;
+#[async_trait(?Send)]
+pub trait SignalToMutable<TSig: 'static>: Signal<Item = TSig> + Sized {
+    async fn to_mutable(self) -> Option<ReadOnlyMutable<TSig>>;
     fn spawn_for_each<TVec, F>(self, update: F) -> MutableVec<TVec>
     where
         TVec: 'static,
         F: FnMut(&MutableVec<TVec>, TSig) + 'static;
 }
 
+#[async_trait(?Send)]
 impl<TSig, Sig> SignalToMutable<TSig> for Sig
 where
-    TSig: Default + 'static,
+    TSig: 'static,
     Sig: Signal<Item = TSig> + Sized + 'static,
 {
-    fn to_mutable(self) -> ReadOnlyMutable<TSig> {
-        let mutable = Mutable::new(TSig::default());
+    async fn to_mutable(self) -> Option<ReadOnlyMutable<TSig>> {
+        let mut s = Box::pin(self.to_stream());
+        let first_value = s.next().await?;
+        let mutable = Mutable::new(first_value);
         let m = mutable.clone();
-        spawn_local(self.for_each(move |value| {
-            m.set(value);
-            async {}
-        }));
-        mutable.read_only()
+        spawn_local({
+            async move {
+                while let Some(value) = s.next().await {
+                    m.set(value);
+                }
+            }
+        });
+        Some(mutable.read_only())
     }
 
     fn spawn_for_each<TVec, F>(self, mut update: F) -> MutableVec<TVec>
