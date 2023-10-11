8d8b3f68a48841efa8abbf9443f92982050d27c7
Simon Bourne <simonbourne@gmail.com>
2023-10-11 17:23:04 +0100

    Another try at fixing cypress timeouts on windows

--- examples/todomvc/e2e/cypress.config.js
+++ examples/todomvc/e2e/cypress.config.js
@@ -4,7 +4,7 @@ module.exports = defineConfig({
   fixturesFolder: false,
   e2e: {
     setupNodeEvents(on, config) {},
-    baseUrl: 'http://localhost:8080',
+    baseUrl: 'http://127.0.0.1:8080',
     defaultCommandTimeout: 60000,
   },
 })
