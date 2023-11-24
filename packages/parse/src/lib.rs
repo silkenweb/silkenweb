8a0ce7d32930aa328878f595ba56a930f5f6ae67
Simon Bourne <simonbourne@gmail.com>
2023-11-24 12:39:05 +0000

    Parse html into intermediate structure

--- packages/parse/src/lib.rs
+++ packages/parse/src/lib.rs
@@ -1,55 +1,91 @@
 // TODO: Test server side
-
-// TODO: Factor out a trait to traverse the tree, creating nodes, and write a
-// proc macro to generate code from a string
-
+// TODO: Write a proc macro to generate code from a string
 // TODO: Doc
 
-use silkenweb::{cfg_browser, dom::Dom, node::Node};
+use silkenweb::{
+    cfg_browser,
+    dom::Dom,
+    node::{
+        element::{Element, GenericElement, Namespace, ParentElement},
+        Node, Text,
+    },
+};
+
+enum DomNode {
+    Element {
+        ns: Namespace,
+        name: String,
+        attributes: Vec<(String, String)>,
+        children: Vec<Self>,
+    },
+    Text(String),
+}
 
 // TODO: Doc
 pub fn html_to_nodes<D: Dom>(html: &str) -> Vec<Node<D>> {
-    arch::html_to_nodes(html)
+    tree_to_nodes(arch::parse_html(html))
+}
+
+fn tree_to_nodes<D: Dom>(nodes: Vec<DomNode>) -> Vec<Node<D>> {
+    nodes
+        .into_iter()
+        .map(|src_node| match src_node {
+            DomNode::Element {
+                ns,
+                name,
+                attributes,
+                children,
+            } => {
+                let mut elem = GenericElement::new(&ns, &name);
+
+                for (name, value) in attributes {
+                    elem = elem.attribute(&name, value);
+                }
+
+                elem.children(tree_to_nodes(children)).into()
+            }
+            DomNode::Text(text) => Text::new(&text).into(),
+        })
+        .collect()
 }
 
 #[cfg_browser(false)]
 mod arch {
     use ego_tree::NodeRef;
     use scraper::Html;
-    use silkenweb::{
-        dom::Dom,
-        node::{
-            element::{Element, GenericElement, Namespace, ParentElement},
-            Node, Text,
-        },
-    };
+    use silkenweb::node::element::Namespace;
 
-    pub fn html_to_nodes<D: Dom>(html: &str) -> Vec<Node<D>> {
+    use crate::DomNode;
+
+    pub fn parse_html(html: &str) -> Vec<DomNode> {
         let fragment = Html::parse_fragment(html);
         tree_to_nodes(&fragment.root_element())
     }
 
-    fn tree_to_nodes<D: Dom>(src_elem: &NodeRef<scraper::node::Node>) -> Vec<Node<D>> {
+    fn tree_to_nodes(src_elem: &NodeRef<scraper::node::Node>) -> Vec<DomNode> {
         src_elem
             .children()
             .filter_map(|src_node| {
                 if let Some(child) = src_node.value().as_element() {
-                    let mut elem = GenericElement::new(
-                        &Namespace::Other(child.name.ns.to_string()),
-                        child.name.local.as_ref(),
-                    );
-
-                    for (name, value) in child.attrs() {
-                        elem = elem.attribute(name, value);
-                    }
-
+                    let ns = Namespace::Other(child.name.ns.to_string());
+                    let name = child.name.local.to_string();
+                    let attributes = child
+                        .attrs()
+                        .map(|(key, value)| (key.to_string(), value.to_string()))
+                        .collect();
                     let children = tree_to_nodes(&src_node);
-                    Some(elem.children(children).into())
+
+                    Some(DomNode::Element {
+                        ns,
+                        name,
+                        attributes,
+                        children,
+                    })
                 } else {
                     src_node
                         .value()
                         .as_text()
-                        .map(|text| Text::new(text).into())
+                        .map(|text| DomNode::Text(text.to_string()))
                 }
             })
             .collect()
@@ -59,15 +95,14 @@ mod arch {
 #[cfg_browser(true)]
 mod arch {
     use silkenweb::{
-        dom::{Dom, Wet},
-        node::{
-            element::{Element, GenericElement, Namespace, ParentElement},
-            Node, Text,
-        },
+        dom::Wet,
+        node::element::{Element, GenericElement, Namespace},
     };
     use wasm_bindgen::JsCast;
 
-    pub fn html_to_nodes<D: Dom>(html: &str) -> Vec<Node<D>> {
+    use crate::DomNode;
+
+    pub fn parse_html(html: &str) -> Vec<DomNode> {
         let tmpl = GenericElement::<Wet>::new(&Namespace::Html, "template");
         let tmpl_elem = tmpl
             .handle()
@@ -78,29 +113,40 @@ mod arch {
         first_child_to_nodes(tmpl_elem.content().first_child())
     }
 
-    fn first_child_to_nodes<D: Dom>(mut child: Option<web_sys::Node>) -> Vec<Node<D>> {
+    fn first_child_to_nodes(mut child: Option<web_sys::Node>) -> Vec<DomNode> {
         let mut nodes = Vec::new();
 
         while let Some(current) = child {
             if let Some(src_elem) = current.dyn_ref::<web_sys::Element>() {
-                let ns = src_elem.namespace_uri().unwrap_or_default();
-                let mut elem = GenericElement::new(&Namespace::Other(ns), &src_elem.local_name());
-                let attributes = src_elem.attributes();
+                let ns = Namespace::Other(src_elem.namespace_uri().unwrap_or_default());
+                let name = src_elem.local_name();
+
+                let src_attributes = src_elem.attributes();
+                let mut attributes = Vec::new();
 
                 for item_index in 0.. {
-                    if let Some(attr) = attributes.item(item_index) {
-                        elem = elem.attribute(&attr.name(), attr.value());
+                    if let Some(attr) = src_attributes.item(item_index) {
+                        attributes.push((attr.name(), attr.value()));
                     } else {
                         break;
                     }
                 }
 
-                nodes.push(
-                    elem.children(first_child_to_nodes(src_elem.first_child()))
-                        .into(),
-                );
+                let children = first_child_to_nodes(src_elem.first_child());
+
+                nodes.push(DomNode::Element {
+                    ns,
+                    name,
+                    attributes,
+                    children,
+                });
             } else if let Some(text) = current.dyn_ref::<web_sys::Text>() {
-                nodes.push(Text::new(text.text_content().as_deref().unwrap_or_default()).into());
+                nodes.push(DomNode::Text(
+                    text.text_content()
+                        .as_deref()
+                        .unwrap_or_default()
+                        .to_string(),
+                ));
             }
 
             child = current.next_sibling();
