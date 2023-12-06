d48b6fc353b4b5cd56990997d2cb0eab427d48eb
Simon Bourne <simonbourne@gmail.com>
2023-12-06 01:46:48 +0000

    Interleaved tests

--- packages/silkenweb/tests/head/mod.rs
+++ packages/silkenweb/tests/head/mod.rs
@@ -1,10 +1,15 @@
-use futures_signals::signal::{Mutable, SignalExt};
+use futures_signals::{
+    signal::{Mutable, SignalExt},
+    signal_vec::{MutableVec, SignalVecExt},
+};
+use itertools::Itertools;
 use silkenweb::{
     document::{Document, DocumentHead},
     dom::{Dom, Dry, Hydro, Wet},
     elements::html::{meta, Meta},
     task::render_now,
 };
+use silkenweb_base::document;
 use silkenweb_macros::cfg_browser;
 use silkenweb_signals_ext::value::Sig;
 use wasm_bindgen_test::{wasm_bindgen_test, wasm_bindgen_test_configure};
@@ -85,10 +90,11 @@ async fn hydro_basic() {
     basic::<Hydro>().await
 }
 
-#[wasm_bindgen_test]
-async fn hydro_interleaved() {
-    interleaved::<Hydro>().await
-}
+// TODO: We need to await `Hydro::MountInHeadOutput`
+// #[wasm_bindgen_test]
+// async fn hydro_interleaved() {
+//     interleaved::<Hydro>().await
+// }
 
 #[wasm_bindgen_test]
 async fn wet_interleaved() {
@@ -98,6 +104,65 @@ async fn wet_interleaved() {
 // We don't test interleaving on `Dry` DOMs as the ordering is different, and
 // the elements are segregated anyway.
 async fn interleaved<D: Document>() {
-    D::unmount_all();
-    // TODO: Implement
+    Wet::unmount_all();
+    Hydro::unmount_all();
+    let head = document::head().unwrap();
+    let existing = head.inner_html();
+    let id1 = "my-id1";
+    let numbers1 = head_vec::<D>(id1);
+    let id2 = "my-id2";
+    let numbers2 = head_vec::<D>(id2);
+    render_now().await;
+    assert_eq!(head.inner_html(), existing.as_str());
+    numbers1.lock_mut().push(0);
+    numbers2.lock_mut().push(0);
+    render_now().await;
+    assert_eq!(
+        head.inner_html(),
+        format!(r#"{existing}{}"#, items([(0, id1), (0, id2)]))
+    );
+    numbers1.lock_mut().push(1);
+    numbers2.lock_mut().push(1);
+    render_now().await;
+    assert_eq!(
+        head.inner_html(),
+        format!(
+            r#"{existing}{}"#,
+            items([(0, id1), (0, id2), (1, id1), (1, id2)])
+        )
+    );
+    numbers1.lock_mut().pop();
+    render_now().await;
+    assert_eq!(
+        head.inner_html(),
+        format!(
+            r#"{existing}{}"#,
+            items([(0, id1), (0, id2), (1, id2)])
+        )
+    );
+    // TODO: More test cases
+}
+
+fn head_vec<D: Document>(id: &str) -> MutableVec<usize> {
+    let numbers = MutableVec::new();
+
+    D::mount_in_head(
+        id,
+        DocumentHead::new().children_signal(
+            numbers
+                .signal_vec()
+                .map(|n| meta().name("description").content(format!("item {n}"))),
+        ),
+    );
+
+    numbers
+}
+
+fn items<'a>(items: impl IntoIterator<Item = (usize, &'a str)>) -> String {
+    items
+        .into_iter()
+        .map(|(n, id)| {
+            format!(r#"<meta name="description" content="item {n}" {HEAD_ID_ATTRIBUTE}="{id}">"#)
+        })
+        .join("")
 }
