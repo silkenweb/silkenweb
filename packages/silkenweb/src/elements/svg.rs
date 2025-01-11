6e791b8e1020fafad33b54704a12e7f9d606026b
Simon Bourne <simonbourne@gmail.com>
2025-01-11 19:41:42 +0000

    Doc for SVG image

--- packages/silkenweb/src/elements/svg.rs
+++ packages/silkenweb/src/elements/svg.rs
@@ -749,6 +749,17 @@ impl<D: Dom> ConditionalProcessing for G<D> {}
 impl<D: Dom> Presentation for G<D> {}
 
 svg_element!(
+    /// The `<image>` SVG element includes images inside SVG documents. It can
+    /// display raster image files or other SVG files.
+    ///
+    /// The only image formats SVG software must support are JPEG, PNG, and 
+    /// other SVG files. Animated GIF behavior is undefined.
+    ///
+    /// SVG files displayed with `<image>` are treated as an image: external 
+    /// resources aren't loaded, :visited styles aren't applied, and they 
+    /// cannot be interactive. To include dynamic SVG elements, try `<use>`
+    /// with an external URL. To include SVG files and run scripts inside them,
+    /// try `<object>` inside of `<foreignObject>`.
     image = {
         dom_type: web_sys::SvgImageElement;
 
