ec4be6bc3d3e65edda05edb66f7e06fd5167991e
Simon Bourne <simonbourne@gmail.com>
2025-01-19 13:30:43 +0000

    Update `tailwind` example with latest `twust`

--- examples/tailwind/src/main.rs
+++ examples/tailwind/src/main.rs
@@ -1,10 +1,21 @@
-use silkenweb::{elements::html::p, mount, node::element::ParentElement, prelude::Element};
-use twust::tw;
+use silkenweb::{
+    elements::html::{div, p},
+    log_panics, mount,
+    node::element::ParentElement,
+    prelude::Element,
+};
+use twust::{tw1, tws1};
 
 fn main() {
+    log_panics();
     mount(
         "app",
-        p().classes([tw!("text-red-600"), tw!("text-2xl")])
-            .text("Hello, world!"),
+        div().children([
+            p().class(tw1!("text-red-600"))
+                .class(tw1!("text-2xl"))
+                .text("Set classes individually"),
+            p().classes(tws1!["text-red-600", "text-2xl"])
+                .text("Set multiple classes at once"),
+        ]),
     );
 }
