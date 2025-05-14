7c651f73d387439ea2fc5ea0d830e6bbc30c05f7
Simon Bourne <simonbourne@gmail.com>
2025-05-14 22:49:25 +0100

    Fix tailwind example

--- examples/tailwind/src/main.rs
+++ examples/tailwind/src/main.rs
@@ -1,7 +1,7 @@
 use silkenweb::{
     elements::html::{div, p},
     log_panics, mount,
-    node::element::ParentElement,
+    node::element::{ParentElement, TextParentElement},
     prelude::Element,
 };
 use twust::{tw1, tws1};
